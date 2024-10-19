9daff88f32cc71a9f826b5d88cad21d0948aa15d
yagyagoel1 <yagyagoel87@gmail.com>
2024-10-19 22:51:28 +0530

    Added the requrest error handler

--- backend/pkg/errorHandler/main.go
+++ backend/pkg/errorHandler/main.go
@@ -1,6 +1,10 @@
 package errorhandler
 
-import "log"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Fatal(err error) {
 	if err != nil {
@@ -8,8 +12,14 @@ func Fatal(err error) {
 	}
 
 }
-func Basic(err error) {
+func Request(err error, c *fiber.Ctx, message string) error {
 	if err != nil {
 		log.Println("an error occured:", err)
 	}
+	c.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+		"success": false,
+		"message": message,
+		"data":    nil,
+	})
+	return err
 }
